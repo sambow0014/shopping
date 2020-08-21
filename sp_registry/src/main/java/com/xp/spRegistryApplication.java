9d38a55fb4843deba7fe806235a3a7be378e514e
Administrator <571379820@qq.com>
2020-08-21 21:00:55 +0800

    创建微服务商城项目的框架

---
+++ sp_registry/src/main/java/com/xp/spRegistryApplication.java
@@ -0,0 +1,14 @@
+package com.xp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringCloudApplication
+public class spRegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(spRegistryApplication.class);
+    }
+}
